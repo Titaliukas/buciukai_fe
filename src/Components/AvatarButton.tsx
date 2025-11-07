980f490794a97c44bbf7504cac713179ebdd0347
Titaliukas <rimtitas@gmail.com>
2025-11-07 15:12:36 +0200

    added menu for avatar button

---
+++ src/Components/AvatarButton.tsx
@@ -0,0 +1,55 @@
+import { Avatar, IconButton, Menu, MenuItem } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import { useState } from 'react';
+
+export default function AvatarButton() {
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const [isLoggedin, setIsLoggedin] = useState<boolean>(true);
+	const open = Boolean(anchorEl);
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	const handleLogOut = () => {
+		setAnchorEl(null);
+		setTimeout(() => {
+			setIsLoggedin(false);
+		}, 200);
+	};
+
+	return (
+		<>
+			<IconButton
+				onClick={handleClick}
+				sx={{ p: 0, '&:focus': { outline: 'none' }, '&:focus-visible': { outline: 'none' } }}
+			>
+				<Avatar>
+					<PersonIcon />
+				</Avatar>
+			</IconButton>
+			<Menu
+				anchorEl={anchorEl}
+				open={open}
+				disableScrollLock
+				onClose={handleClose}
+				transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+				anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+			>
+				{isLoggedin ? (
+					<>
+						<MenuItem onClick={handleClose}>Profilio nustatymai</MenuItem>
+						<MenuItem onClick={handleClose}>Mano rezervacijos</MenuItem>
+						<MenuItem onClick={handleClose}>Admin puslapis</MenuItem>
+						<MenuItem onClick={handleLogOut}>Atsijungti</MenuItem>
+					</>
+				) : (
+					<>
+						<MenuItem onClick={handleClose}>Prisijungti</MenuItem>
+					</>
+				)}
+			</Menu>
+		</>
+	);
+}
